6f0ec9ff52d5eaed4c8716ce77426cee3eb0b39f
dalejandrorincon <dalejandrorinconp@gmail.com>
2024-01-04 07:09:12 -0500

    Seed y paginación

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { CommomModule } from 'src/commom/commom.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [PokemonModule, CommomModule],
 })
 export class SeedModule {}
